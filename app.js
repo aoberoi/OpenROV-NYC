8c3abebaeb5cb506e41f6a15b9c4795ca8a28bad
Ankur Oberoi <aoberoi@gmail.com>
2012-12-14 00:56:54 -0800

    initial commit
    * sets up app.js with good defaults
    * master routing set up in routes/index.js
    * sample resourceful set of routes with views

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path');
+
+var app = express();
+
+/*
+ * Settings
+ */
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+/*
+ * Middleware
+ */
+// Logging
+app.use(express.logger( app.get('env') === 'development' ? 'dev' : 'default' ));
+// Set up req.body
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+// Cookies
+app.use(express.cookieParser('secret'));
+app.use(express.session()); // NOTE: consider using secure cookie if served over tls
+// Router
+app.use(app.router);
+// Static files
+app.use(express.favicon());
+app.use(express.static(path.join(__dirname, 'public')));
+// Error handling
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+/*
+ * Router
+ */
+var routes = require('./routes')(app);
+
+/*
+ * Server
+ */
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+/*
+ * External interface
+ */
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+/*
+ * Global Routing
+ */
+
+function index(req, res) {
+  res.render('index', { title : 'Express' });
+}
+
+module.exports = function(app) {
+  app.get('/', index);
+
+  // Mount a resource
+  require('./resource').mount(app, '/resources');
+};
