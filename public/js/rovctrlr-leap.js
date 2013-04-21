7477cfe0a9290cf39d20a75493c345781ab8f14a
Ankur Oberoi <aoberoi@gmail.com>
2013-04-21 15:10:53 -0400

    asfdasdf

--- public/js/rovctrlr-leap.js
+++ public/js/rovctrlr-leap.js
@@ -17,7 +17,7 @@ LeapController.prototype.start = function() {
     "yaw" : 0,
     "throttle" : 0
   };
-  window.setInterval(_.bind(this.sendCommands, this), 25);
+  window.setInterval(_.bind(this.sendCommands, this), 100);
 }
 
 LeapController.prototype.stop = function() {
